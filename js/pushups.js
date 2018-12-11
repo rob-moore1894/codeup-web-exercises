e813056a3ac5f7f1ed6f22cb17a124d987b0f379
Robert Moore <rob@Roberts-MacBook-Pro.local>
2018-12-11 09:48:19 -0600

    Completed warm up exercises

--- js/pushups.js
+++ js/pushups.js
@@ -20,13 +20,21 @@ console.log(getLowest([1, 2, -11]));
 
 // Function sumAllNumbers() that takes in a two-dimensional array of numbers and returns the sum of all numbers added together
 
-
 var arrayOfNumbers = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ];
 
+var sum = 0;
+
+function sumAllNumbers(array) {
+    for (var i = 0; i < array.length; i++) {
+        for (var j = 0; j < array[i].length; j++) {
+            sum += array[i][j];
+        }
+    } return sum;
+}
 
 console.log(sumAllNumbers(arrayOfNumbers));
 console.log(sumAllNumbers([[0, 1, 2, 0],[5, 0],[6, 7, 8]]));
