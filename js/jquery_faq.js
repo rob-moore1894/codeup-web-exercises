9fff11b120e18d1443bb9823cef53cebab5c5e02
Robert Moore <rob@Roberts-MacBook-Pro.local>
2018-12-12 15:39:09 -0600

    Updated file with jQuery traversing.  Bonus not complete

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -2,10 +2,33 @@
 
 $(document).ready(function(){
 
-    $("dt").click(function(event) {
-        $("dd").removeClass()
+    $("#toggler").click(function() {
+        highlightLastItem();
+    });
+
+    $('dt').click(function(){
+       $(this).next().toggleClass('invisible');
+    });
+
+    function highlightLastItem() {
+        $('ul').each(function () {
+            $(this).children().last().css('background-color', '#FF0')
+        });
+    };
+
+    $('h3').click(function (){
+        $(this).next().children().css('font-weight', 'bold');
+    });
+
+    $('li').click(function (){
+        $(this).parent().children().first().css('color', 'blue');
     });
 
 
+    //     The rules are the following:
+    //
+    //     The left frame swaps to the right and takes the image from the frame in the center.
+    //     The center frame swaps randomly to either the left or right.
+    //     The right frame swaps to the left and takes the image from the frame in the center.
 
 });
