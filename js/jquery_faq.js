7d544c508ce851822958109463e71571b62fc731
Robert Moore <rob@Roberts-MacBook-Pro.local>
2018-12-12 16:54:32 -0600

    Updated with additional jQuery to add effects

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -2,33 +2,54 @@
 
 $(document).ready(function(){
 
+    // Button highlights the last item in each ul
     $("#toggler").click(function() {
         highlightLastItem();
-    });
-
-    $('dt').click(function(){
-       $(this).next().toggleClass('invisible');
     });
 
     function highlightLastItem() {
         $('ul').each(function () {
             $(this).children().last().css('background-color', '#FF0')
         });
-    };
+    }
 
-    $('h3').click(function (){
-        $(this).next().children().css('font-weight', 'bold');
+    //Shows the dd from each dt
+    $('dt').click(function(){
+       $(this).next().toggleClass('invisible');
     });
 
+    // The uls next to each h3 that contain facts about each park should start out hidden, and when the corresponding h3 is clicked, the display of the ul should be toggled with a sliding effect.
+    $('h3').click(function (){
+        $(this).next().slideToggle().css('font-weight', 'bold');
+    });
+
+    // Clicking on any li will change the font color of the first li within the selected ul to blue
     $('li').click(function (){
         $(this).parent().children().first().css('color', 'blue');
     });
 
+    // Hides the div that contains the Newsletter
+    $('span').click(function(){
+       $(this).parent().hide();
+    });
 
-    //     The rules are the following:
+    // After the user has been on the page for 8 seconds, an h2 should fade in that asks the user to register for the site.
+    setInterval(function(){
+        $('h2').fadeIn();
+    }, 7000);
+
+
+    //     The Bonus' rules are the following:
     //
     //     The left frame swaps to the right and takes the image from the frame in the center.
+    $('#pic1').dblclick(function(){
+        $(this).slideToggle();
+    });
+
     //     The center frame swaps randomly to either the left or right.
+
+
     //     The right frame swaps to the left and takes the image from the frame in the center.
+
 
 });
