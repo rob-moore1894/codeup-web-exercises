13126265ed62f75ddd07332e9ab6e51b72130d29
rob-moore1894 <moore.re@gmail.com>
2018-12-16 22:30:05 -0600

    Changed from one day to three day forecast

--- js/weather_map.js
+++ js/weather_map.js
@@ -9,35 +9,37 @@ $(document).ready(function() {
         if (city !== ''){
 
             $.ajax({
-                url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial' + '&APPID=e607a180955b52a1622535c6a426c5b4',
+                url: 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial' + '&APPID=e607a180955b52a1622535c6a426c5b4',
                 type: "GET",
                 dataType: "jsonp",
                 success: function(data){
-                    var widget = show(data);
-                    var femalePic = femalePicture(data);
-                    var malePic = malePicture(data);
-                    var recenter = recenterMap(data);
+                    var location = cityLocation(data);
+                    var current24 = current(data);
+                    var next24 = nextDay(data);
+                    var next48 = thirdDay(data);
+                    // var recenter = recenterMap(data);
 
                     console.log(data);
 
-                    $('#show').html(widget);
-                    $('#femalePic').html(femalePic);
-                    $('#malePic').html(malePic);
-                    console.log(recenter);
+                    $('#cityName').html(location);
+                    $('#current24').html(current24);
+                    $('#next24').html(next24);
+                    $('#next48').html(next48);
+                    // console.log(recenter);
                     $('#city').val('');
                 }
             });
 
         }else {
             $('.form-control').css('border', '2px solid red');
-            $('#femalePic').html('Field Cannot Be Empty!')
+            $('#cityName').html('Field Cannot Be Empty!')
         }
 
     });
 
     var mapOptions = {
         // Set the zoom level
-        zoom: 5,
+        zoom: 10,
 
         // This sets the center of the map
         center: {
@@ -53,54 +55,55 @@ $(document).ready(function() {
     var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
     // // Init geocoder object
-    // var geocoder = new google.maps.Geocoder();
+    var geocoder = new google.maps.Geocoder();
 
     // // Geocode our address
-    // geocoder.geocode({ "address":  }, function(results, status) {
-    //
-    //     // Check for a successful result
-    //     if (status == google.maps.GeocoderStatus.OK) {
-    //
-    //         // Recenter the map over the address
-    //         map.setCenter(results[0].geometry.location);
-    //     } else {
-    //
-    //         // Show an error message with the status if our request fails
-    //         alert("Geocoding was not successful - STATUS: " + status);
-    //     }
-    // });
-    //
-    // Adding marker to the map
+    geocoder.geocode({ "address": data.city.coord.lat, data.city.coord.lon}, function(results, status) {
+
+        // Check for a successful result
+        if (status == google.maps.GeocoderStatus.OK) {
+
+            // Recenter the map over the address
+            map.setCenter(results[0].geometry.location);
+        } else {
+
+            // Show an error message with the status if our request fails
+            alert("Geocoding was not successful - STATUS: " + status);
+        }
+    });
+
+    //Adding marker to the map
     // var marker = new google.maps.Marker({
     //     position: {
-    //         lat: 29.4858551,
-    //         lng: -98.5083507,
+    //         lat: newLat,
+    //         lng: newLon,
     //     },
     //     map: map,
     //     animation: google.maps.Animation.DROP,
     // });
 });
 
-function show(data) {
-    return "<h3><strong>City</strong>: " + data.name + "</h3>" + "<h3><strong>Weather</strong>: " + data.weather[0].main + "</h3>" +
-        "<h3><strong>Description</strong>: " + data.weather[0].description + "</h3>" + "<h3><strong>Temperature</strong>: " + data.main.temp + "</h3>" +
-        "<h4>Min: </h4>" + data.main.temp_min + "<h4>Max: </h4>" + data.main.temp_max
+function cityLocation(data) {
+    return "<h3>" + data.city.name + "</h3>"
 }
 
-function femalePicture(data) {
-    if (data.main.temp <= 60) {
-        return "<img id='cold-female' src='images/female_cold.jpg' >"
-    }
+function current(data) {
+    return "Current 24" + "<h3>Weather: " + data.list[1].weather[0].main + "</h3>" +
+        "<h3><strong>Description</strong>: " + data.list[1].weather[0].description + "</h3>" + "<h3><strong>Temperature</strong>: " + data.list[1].main.temp + "</h3>" + "<h4>Min: </h4>" + data.list[1].main.temp_min + "<h4>Max: </h4>" + data.list[1].main.temp_max
+}
+//
+function nextDay(data) {
+    return "Next 24" + "<h3>Weather: " + data.list[9].weather[0].main + "</h3>" +
+        "<h3><strong>Description</strong>: " + data.list[9].weather[0].description + "</h3>" + "<h3><strong>Temperature</strong>: " + data.list[9].main.temp + "</h3>" + "<h4>Min: </h4>" + data.list[9].main.temp_min + "<h4>Max: </h4>" + data.list[9].main.temp_max
  }
 
- function malePicture(data) {
-    if (data.main.temp <= 60) {
-        return "<img id='cold-male' src='images/male_cold.jpg'>"
-    }
+ function thirdDay(data) {
+     return "Next 48" + "<h3>Weather: " + data.list[17].weather[0].main + "</h3>" +
+         "<h3><strong>Description</strong>: " + data.list[17].weather[0].description + "</h3>" + "<h3><strong>Temperature</strong>: " + data.list[17].main.temp + "</h3>" + "<h4>Min: </h4>" + data.list[17].main.temp_min + "<h4>Max: </h4>" + data.list[17].main.temp_max
  }
 
- function recenterMap(data) {
-    marker.position.lat = data.coord.lat;
-    marker.position.lng = data.coord.lon;
- }
+ // function recenterMap(data) {
+ //    var newLat = data.city.coord.lat;
+ //    var newLon = data.city.coord.lon;
+ // }
 
